f5ab017126e818ae3602c58bb01a4100ad3035a5
rian-06 <108826649+rian-06@users.noreply.github.com>
2022-10-04 17:45:07 -0300

    template
    php, index, style e etc, base do jornal toda feita

---
+++ includes/_header.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <link rel="stylesheet" href="./assests/css/style.css">
+    <title>Jornal da escola</title>
+  </head>
+  <body>
+    
+    <header>
+    <nav>
+        <ul>
+        <li><a href="./index.php">index</a></li>
+        <li><a href="./editoria.php">editoria</a></li>
+        <li><a href="./noticia.php">Notícia</a></li>
+    </ul>
+    </nav>
+    </header>
+</body>
+<html>
