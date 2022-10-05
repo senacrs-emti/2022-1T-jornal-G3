503624373ef710d8863f4cfe1512d9194aadc6fe
rian-06 <riamcontaaleatoria@gmail.com>
2022-10-05 15:40:22 -0300

    header começo
    n terminei falta algumas coisas na ehader

--- includes/_header.php
+++ includes/_header.php
@@ -11,7 +11,23 @@
   </head>
   <body>
     
-    <header>
+    <header class=".container-lg">
+
+    <nav class="navbar navbar-dark bg-dark">
+    <a class="navbar-brand" href="#">Sport Hub news</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+      <a class="nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+      <a class="nav-link" href="#">Features</a>
+      <a class="nav-link" href="#">Pricing</a>
+      <a class="nav-link disabled">Disabled</a>
+    </div>
+  </div>
+    </nav>
+
     <nav>
         <ul>
         <li><a href="./index.php">index</a></li>
