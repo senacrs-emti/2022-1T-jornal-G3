2083d7bbc225a5ba4aba8401216850addcef613c
rian-06 <riamcontaaleatoria@gmail.com>
2022-10-26 14:06:51 -0300

    Página da noticia model
    adicionei imagem e lorem na pagina de noticia, provavelmente terminada

--- index.php
+++ index.php
@@ -3,13 +3,13 @@
 include_once './includes/_header.php';
 ?>
 <main class="col-lg-8 col-12">
-        <h1>Index</h1>
+
 <div class="card bg-dark m-5 text-white card-noticia" style="width: 45rem;">
   <img src="https://www.lance.com.br/files/article_main/uploads/2022/10/17/634d8550da4e0.jpeg" class="card-img-top col-11 ml-4 mr-4 mt-2 card-image" alt="...">
   <div class="card-body">
     <h5 class="card-title">Título notícia </h5>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Leia mais</a>
+    <a href="./noticia.php" class="btn btn-primary">Leia mais</a>
   </div>
 </div>
 
@@ -18,7 +18,7 @@ include_once './includes/_header.php';
   <div class="card-body">
     <h5 class="card-title">Título notícia </h5>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Leia mais</a>
+    <a href="./noticia.php" class="btn btn-primary">Leia mais</a>
   </div>
 </div>
 
@@ -27,7 +27,7 @@ include_once './includes/_header.php';
   <div class="card-body">
     <h5 class="card-title">Título notícia </h5>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Leia mais</a>
+    <a href="./noticia.php" class="btn btn-primary">Leia mais</a>
   </div>
 </div>
 
@@ -36,7 +36,7 @@ include_once './includes/_header.php';
   <div class="card-body">
     <h5 class="card-title">Título notícia </h5>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Leia mais</a>
+    <a href="./noticia.php" class="btn btn-primary">Leia mais</a>
   </div>
 </div>
 
@@ -45,7 +45,7 @@ include_once './includes/_header.php';
   <div class="card-body">
     <h5 class="card-title">Título notícia </h5>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Leia mais</a>
+    <a href="./noticia.php" class="btn btn-primary">Leia mais</a>
   </div>
 </div>
     </main>
