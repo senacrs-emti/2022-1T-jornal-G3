57f07816713ba6ce5a331577c679d92f64752c5f
a <108826649+rian-06@users.noreply.github.com>
2022-11-09 15:23:40 -0300

    textos adicionados em files
    adicionei os textos a files php, adicionado cronica, artigo de opinião e notícia

---
+++ cronica-jogo-do-gremio.php
@@ -0,0 +1,5 @@
+<h1>Jogo do Grêmio</h1>
+<h6>Autor: Rian Garcia Pereira</h6>
+<p>Eu lembro bem pouco da primeira vez que fui em um jogo do grêmio, eu era bem pequeno, ainda era no estádio Olímpico, que foi fechado em 2014. Nesse dia fui com minha dinda que já morava no Rio de Janeiro, então eu não via ela muito, junto com ela foi minha mãe, meu irmão e um tio, nem se quer me lembro do placar do jogo ou contra quem era.</p>
+<p>Mas eu me lembro de que durante o jogo houve um gol, e todos começaram a gritar e pular, minha dinda me segurou e ficou gritando, me levantou até para tentar me fazer ver o que estava acontecendo, eu nunca tinha visto algum jogo de futebol nem na televisão, não entendi nada daquela gritaria, fiquei assustado, só comecei a gostar de futebol anos depois em 2017.</p>
+<p>Assim como esse foi o primeiro jogo que eu vi, tudo na vida tem uma primeira vez, me lembro de pouquíssimas, não lembro da primeira vez que comi chocolate ou quando bebi refrigerante, não lembro dessas pois algumas coisas são mais memoráveis que as outras, talvez por acontecerem menos ou apenas serem mais especiais.</p>
