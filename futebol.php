34e6c7410a941c31dbe1b8a4b936142b937536d4
Adriely <108826649+rian-06@users.noreply.github.com>
2022-11-22 17:10:21 -0300

    página do futebol sendo atualizada
    ainda há alterações para fazer na editoria de futebol, estou terminando

--- futebol.php
+++ futebol.php
@@ -5,13 +5,46 @@ include_once './includes/_header.php';
 <main class="col-lg-9 col-12">
     <img class="imagemeditoria mt-3" src="https://cdn-icons-png.flaticon.com/512/53/53283.png" alt="">
     <h1 class="text-editoria">Futebol</h1>
+<hr>
 
-    <div class="card bg-dark m-5 text-white card-noticia col-10" style="width: 45rem;">
-  <img src="https://www.lance.com.br/files/article_main/uploads/2022/10/17/634d8550da4e0.jpeg" class="card-img-top col-11 ml-4 mr-4 mt-2 card-image" alt="...">
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100 carousel-imgs" src="./contents/argentina-perdeu.jpg" alt="First slide">
+      <div class="carousel-caption d-none d-md-block">
+    <h5>Argentina perde contra Arábia Saudita</h5>
+    <p>Depois de uma virada da Arábia, Argentina tem uma derrot após 1.300 dias</p>
+  </div>
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100 carousel-imgs" src="..." alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100 carousel-imgs" src="..." alt="Third slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+    
+    <div class="card bg-dark m-5 text-white card-noticia col-12" style="width: 45rem;">
+    <h1 class="mt-1 ml-5">NOTÍCIA</h1>
+  <img src="./contents/benzema-e-a-bola-de-ouro.jpg" class="card-img-top col-11 ml-4 mr-4 mt-2 card-image" alt="...">
   <div class="card-body">
-    <h5 class="card-title">Título notícia (relacionada ao esporte) </h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="./noticia.php" class="btn btn-primary">Leia mais</a>
+    <h5 class="card-title">Karim Benzema é o melhor do mundo </h5>
+    <p class="card-text">O atacante do Real Madrid ficou em primeiro na premiação de melhor do mundo da France Football</p>
+    <a href="./noticia-karim-benzema.php" class="btn btn-light">Leia mais</a>
   </div>
 </div>
 
