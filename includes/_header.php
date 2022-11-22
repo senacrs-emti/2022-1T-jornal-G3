3178773a27cd84525ed88581d673a65c6e9501e3
Adriely <108826649+rian-06@users.noreply.github.com>
2022-11-22 13:57:16 -0300

    só mudei o nome que fica na guia
    só coloquei o nome de sporthub news no nome do site, e falta colocar as noticias, artigos de opiniões e crônicas

--- includes/_header.php
+++ includes/_header.php
@@ -7,7 +7,7 @@
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
     <link rel="stylesheet" href="./assests/css/style.css">
-    <title>Jornal da escola</title>
+    <title>Sport Hub News</title>
   </head>
   <body class="container-fluid">
 
