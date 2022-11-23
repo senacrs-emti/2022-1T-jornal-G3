b9e17db4535be6ffa0b3f87c0a60a76cbd2a606d
Framezinho <108826458+Caua-guadagnin@users.noreply.github.com>
2022-11-23 14:08:38 -0300

    CAUA changes 69

---
+++ noticia-f1.php
@@ -0,0 +1,27 @@
+<?php
+//include header
+include_once './includes/_header.php';
+?>
+<main class="col-lg-9 col-12">
+    <h1 class="noticia-text mt-4">Aposentadoria de Sebastian Vettel</h1>
+    <h3 class="noticia-text mt-3">Famoso piloto da categoria irá se aposentar do esporte</h3>
+    <h5 class="noticia-text mt-3">Cauã Guadagnin Silveira</h5>
+
+    <img class="col-12 image-noticia" src="https://esporteamotor.com.br/wp-content/uploads/2022/10/Aposentadoria-ficando-um-pouco-mais-real-a-medida-que-o.jpg" alt="">
+
+    <p class="p-noticia">Sebastian Vettel, famoso piloto de formula um irá se aposentar no fim da temporada de 2022. Vettel anunciou sua aposentadoria em julho deste ano após 16 temporadas na categoria e quatro títulos mundiais. Em 2021 Seb conquistou o único pódio da Aston Martin, no GP do Azerbaijão – também foi sua última vez no pódio até então.</p>
+
+    <p class="p-noticia">No meio do ano (28/07) Sebastian anunciou o fim de sua carreira de 15 anos, com seu contrato atual com a Aston Martin concluindo em 2022. E para o alemão, a realidade de deixar a formula 1 está gradualmente sendo assimilada na contagem regressiva das corridas restantes, sendo sua última no GP de Abu Dhabi.</p>
+
+    <p class="p-noticia">Vettel estava visivelmente emocionado ao ter que dizer adeus para sua pista favorita do calendário, Suzuka. E, no último fim de semana, em Austin, ele admitiu que sentirá falta da adrenalina e de lutar roda a roda com outros corredores, como fez com Kevin Magnussen na última volta da corrida.</p>
+
+    <p class="p-noticia">Com certeza, a carreira de Sebastian Vettel foi uma das mais impactantes e representativas da história da formula 1, porém tudo chega ao fim. Após mais de uma década e meia e aos 35 anos de idade, Vettel fica no retrovisor.</p>
+
+</main>
+<?php
+// include do aside
+include_once './includes/_aside.php';
+
+//include do footer
+include_once './includes/_footer.php';
+?>
