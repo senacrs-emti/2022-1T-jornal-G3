6b7e79158bdeb1632abfb6fb1bdc4f89255c0020
isabelalacerda1234 <108826418+Jonasuwu@users.noreply.github.com>
2022-11-23 15:25:10 -0300

    aside feitin

--- includes/_aside.php
+++ includes/_aside.php
@@ -1,25 +1,25 @@
 <aside class="col-lg-3 col-12">
 <div class="card ml-4" style="width: 15rem;">
-  <img src="https://s2.glbimg.com/6N6UKj1cx5Nv6hZf3BmiHwx7Y1A=/0x0:1080x645/924x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_cf9d035bf26b4646b105bd958f32089d/internal_photos/bs/2022/Z/F/C923T1TlORxpzmAb8lAg/ferraro.jpg" class="card-img-top card-image" alt="...">
+  <img src="http://localhost:8080/2022-1T-jornal-G3/contents/benzema-e-a-bola-de-ouro.jpg" alt="...">
   <div class="card-body">
-    <p class="card-text">Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...</p>
-    <button type="button" class="btn btn-success">Leia</button>
+    <h5 class="card-text">Karim Benzema é o melhor do mundo</h5>
+    <a href="./noticia-karim-benzema.php" class="btn btn-primary btn-success">Leia</a>
   </div>
 </div>
 
 <div class="card ml-4" style="width: 15rem;">
-  <img src="https://s2.glbimg.com/6N6UKj1cx5Nv6hZf3BmiHwx7Y1A=/0x0:1080x645/924x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_cf9d035bf26b4646b105bd958f32089d/internal_photos/bs/2022/Z/F/C923T1TlORxpzmAb8lAg/ferraro.jpg" class="card-img-top card-image" alt="...">
+  <img src="https://classic.exame.com/wp-content/uploads/2022/10/GettyImages-1435299459.jpg?quality=70&strip=info&w=1024" alt="...">
   <div class="card-body">
-    <p class="card-text">Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...</p>
-    <button type="button" class="btn btn-success">Leia</button>
+    <h5 class="card-text">CHARLES DO BRONX PERDE POR MEIO DE FINALIZAÇÃO CONTRA ISLAM MAKHACHEV.</h5>
+    <a href="./noticia-ufc.php" class="btn btn-primary btn-success">Leia</a>
   </div>
 </div>
 
 <div class="card ml-4" style="width: 15rem;">
-  <img src="https://s2.glbimg.com/6N6UKj1cx5Nv6hZf3BmiHwx7Y1A=/0x0:1080x645/924x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_cf9d035bf26b4646b105bd958f32089d/internal_photos/bs/2022/Z/F/C923T1TlORxpzmAb8lAg/ferraro.jpg" class="card-img-top card-image" alt="...">
+  <img src="https://esporteamotor.com.br/wp-content/uploads/2022/10/Aposentadoria-ficando-um-pouco-mais-real-a-medida-que-o.jpg" class="card-img-top card-image" alt="...">
   <div class="card-body">
-    <p class="card-text">Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...</p>
-    <button type="button" class="btn btn-success">Leia</button>
+    <h5 class="card-text">Aposentadoria de Sebastian Vettel</h5>
+    <a href="./noticia-f1.php" class="btn btn-primary btn-success">Leia</a>
   </div>
 </div>
 </aside>
