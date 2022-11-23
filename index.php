484c92520d64a08b98290498ff2ae923ad058f3b
isabelalacerda1234 <108826418+Jonasuwu@users.noreply.github.com>
2022-11-23 13:50:01 -0300

    Commit

--- index.php
+++ index.php
@@ -14,11 +14,11 @@ include_once './includes/_header.php';
 </div>
 
 <div class="card bg-dark m-5 text-white card-noticia col-10" style="width: 45rem;">
-  <img src="https://www.lance.com.br/files/article_main/uploads/2022/10/17/634d8550da4e0.jpeg" class="card-img-top col-11 ml-4 mr-4 mt-2 card-image" alt="...">
+  <img src="https://www.mktesportivo.com/wp-content/uploads/2022/04/luva_pedreiro_amazon_2.jpg" class="card-img-top col-11 ml-4 mr-4 mt-2 card-image" alt="...">
   <div class="card-body">
-    <h5 class="card-title">Título notícia </h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="./noticia.php" class="btn btn-primary">Leia mais</a>
+    <h5 class="card-title">Amazon irá transmitir a NBA e usa Luva de Pedreiro como propaganda</h5>
+    <p class="card-text">O release enviado pelo serviço de streaming afirma que a plataforma terá quatro jogos exclusivos por semana.</p>
+    <a href="./noticiabasket.php" class="btn btn-primary">Leia mais</a>
   </div>
 </div>
 
