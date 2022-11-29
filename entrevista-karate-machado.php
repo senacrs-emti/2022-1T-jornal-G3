86e3c679281525fc2a86e2b21b9d6c6fb923b3b4
Jonasuwu <108826418+Jonasuwu@users.noreply.github.com>
2022-11-29 17:13:55 -0300

    entrevista karate

---
+++ entrevista-karate-machado.php
@@ -0,0 +1,58 @@
+<?php
+//include header
+include_once './includes/_header.php';
+?>
+<main class="col-lg-9 col-12">
+    <h1 class="noticia-text mt-4">Entrevista com João Pedro Machado - Karatê</h1>
+    <h3 class="noticia-text mt-3">Entrevista com João Pedro Machado, que pratica karatê desde os 6 anos.</h3>
+    <h5 class="noticia-text mt-3">Autor: Joao Vitor</h5>
+
+    <img class="col-12 image-noticia" src="https://casajardimdecor.com/wp-content/uploads/2019/05/kimono-karate-gi-traditionnal-kwon.jpg" alt="">
+<br>
+<br>
+    <p class="p-noticia"><b>• Você quer ser Lutador profissional de Karate?</b></p>
+
+    <p class="p-noticia">"Não."</p>    
+
+    <p class="p-noticia"><b>• Ama o esporte ou encara somente Como um possível trabalho/profissão?</b></p>
+
+    <p class="p-noticia">"Amo o esporte."</p>
+
+    <p class="p-noticia"><b>• Qual é sua principal motivação?</b></p>
+
+    <p class="p-noticia">"Defender os fracos e ficar igual a Bruce Lee."</p>
+
+    <p class="p-noticia"><b>• Você se espelha em alguém?</b></p>
+
+    <p class="p-noticia">"Bruce Lee e Jaime Sandel"</p>
+
+    <p class="p-noticia"><b>• Você gosta muito de Esportes?</b></p>
+
+    <p class="p-noticia">"Sim."</p>
+
+    <p class="p-noticia"><b>• Gosta de treinar?</b></p>
+
+    <p class="p-noticia">"Sim."</p>
+
+    <p class="p-noticia"><b>• Desde pequeno já lutava?</b></p>
+
+    <p class="p-noticia">"Sim, desde os 6 anos de idade."</p>
+
+    <p class="p-noticia"><b>• Você era bom no karate na infância?</b></p>
+
+    <p class="p-noticia">"Não, mas me destacava"</p>
+
+    <p class="p-noticia"><b>• "Sua família acompanha as suas lutas e te apoia?</b></p>
+
+    <p class="p-noticia">"Todos apoiam, mas não assistem as lutas."</p>
+
+
+
+</main>
+<?php
+// include do aside
+include_once './includes/_aside.php';
+
+//include do footer
+include_once './includes/_footer.php';
+?>
