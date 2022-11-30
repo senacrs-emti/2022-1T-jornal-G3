0f366ac1197fe1a2f81f956eae24878da210a14f
betoleitao5 <108826649+rian-06@users.noreply.github.com>
2022-11-30 15:19:37 -0300

    card F1
    arrumei os card da f1, deixando eles maiores  e etc

--- formula1.php
+++ formula1.php
@@ -4,32 +4,32 @@ include_once './includes/_header.php';
 ?>
 <main class="col-lg-9 col-12">
     <img class="imagemeditoria mt-3" src="https://logodownload.org/wp-content/uploads/2016/11/formula-1-logo-1-1.png" alt="">
-    <h1 class="text-editoria">Formula 1</h1>
+    <h1 class="text-editoria">Fórmula 1</h1>
 
-    <div class="card bg-dark m-5 text-white card-noticia col-10" style="width: 45rem;">
-  <img src="https://esporteamotor.com.br/wp-content/uploads/2022/09/Sebastian-Vettel-questionado-sobre-o-teaser-do-Instagram-de-um.jpg" class="card-img-top col-11 ml-4 mr-4 mt-2 card-image" alt="...">
+    <div class="card bg-dark mt-5 mb-5 text-white card-noticia col-12">
+  <img class="mt-3" src="https://esporteamotor.com.br/wp-content/uploads/2022/10/Aposentadoria-ficando-um-pouco-mais-real-a-medida-que-o.jpg" class="card-img-top col-11 ml-4 mr-4 mt-2 card-image" alt="...">
   <div class="card-body">
     <h5 class="card-title">Aposentadoria de Sebastian Vettel</h5>
-    <p class="card-text">Famoso piloto da categoria irá se aposentar</p>
-    <a href="./noticia-f1.php" class="btn btn-primary">Leia mais</a>
+    <p class="card-text">Noticia sobre a aposentadoria do famoso piloto Sebastian Vettel</p>
+    <a href="./noticia-f1.php" class="btn btn-light btn-block">Leia mais</a>
   </div>
 </div>
 
-<div class="card bg-dark m-5 text-white card-noticia col-10" style="width: 45rem;">
+<div class="card bg-dark mt-5 mb-5 text-white card-noticia col-12">
   <img src="https://diariodonordeste.verdesmares.com.br/image/contentid/policy:1.3302421:1668805008/FhefoSdXkAEbfa4_Easy-Resize.com.jpg?f=16x9&$p$f=991adb3" class="card-img-top col-11 ml-4 mr-4 mt-2 card-image" alt="...">
   <div class="card-body">
     <h5 class="card-title">Os melhores pilotos da Formula 1</h5>
     <p class="card-text">artigo de opinião sobre os melhores pilotos da categoria</p>
-    <a href="./artigo-opiniao-f1.php" class="btn btn-primary">Leia mais</a>
+    <a href="./artigo-opiniao-f1.php" class="btn btn-light btn-block">Leia mais</a>
   </div>
 </div>
 
-<div class="card bg-dark m-5 text-white card-noticia col-10" style="width: 45rem;">
+<div class="card bg-dark mt-5 mb-5 text-white card-noticia col-12">
   <img src="https://cdn-1.motorsport.com/images/amp/0mNdrpA2/s6/f1-european-gp-1993-ayrton-sen.jpg" class="card-img-top col-11 ml-4 mr-4 mt-2 card-image" alt="...">
   <div class="card-body">
     <h5 class="card-title">Crônica de Senna</h5>
     <p class="card-text">Crônica sobre uma das corridas do falecido piloto</p>
-    <a href="./cronica-de-senna.php" class="btn btn-primary">Leia mais</a>
+    <a href="./cronica-de-senna.php" class="btn btn-light btn-block">Leia mais</a>
   </div>
 </div>
 
